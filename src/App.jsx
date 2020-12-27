3a3bd8afa7bffa76e5470b7e8cefd899dd8f77d7
github cjl248 <github cjl248@cornell.edu>
2020-12-27 16:55:27 -0500

    Added node-sass and material-ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.scss';
+import Header from './components/header/Header.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
